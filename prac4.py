70be975746cb5158ebcc06de9f702b7f9fe76556
comeeasy <zz1236zz@naver.com>
2021-04-17 23:32:32 +0900

    update

--- prac4.py
+++ prac4.py
@@ -10,6 +10,98 @@ from torch.autograd import Variable
 import matplotlib.pyplot as plt
 from torch.optim import lr_scheduler
 
+import argparse
+
+class my_ResNet(nn.Module):
+    def __init__(self):
+        super(my_ResNet, self).__init__()
+        self.start_layer = nn.Sequential(
+            nn.Conv2d(3, 16, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(),
+            nn.MaxPool2d(2, 2)
+        )
+        self.layer1 = block3x3(16, 16)
+
+        self.layer2 = bottleneck3x3(16, 32, stride=1)
+        self.layer2_res = bottleneck1x1(16, 32, stride=1)
+
+        self.layer3 = block3x3(32, 32)
+
+        self.layer4 = bottleneck3x3(32, 64, stride=2)
+        self.layer4_res =bottleneck1x1(32, 64, stride=2)
+
+        self.layer5 = block3x3(64, 64)
+
+        self.layer6 = bottleneck3x3(64, 128, stride=2)
+        self.layer6_res = bottleneck1x1(64, 128, stride=2)
+
+        self.layer7 = block3x3(128, 128)
+
+        self.layer8 = bottleneck3x3(128, 256)
+        self.layer8_res = bottleneck1x1(128, 256)
+
+        self.layer9 = block3x3(256, 256)
+
+        self.avg_pool = nn.Sequential(
+            nn.AvgPool2d(2, 2)
+        )
+        self.fc_layer = nn.Sequential(
+            nn.Flatten(),
+            nn.Linear(128 * 2 * 2, 128),
+            nn.ReLU(),
+            nn.Dropout(p=0.2),
+            nn.Linear(128, 10)
+        )
+        self.relu = nn.ReLU()
+
+    def forward(self, x):
+        x = self.start_layer(x)
+        x = self.relu(self.layer1(x.clone()) + x)
+
+        x = self.relu(self.layer2(x.clone()) + self.layer2_res(x))
+        x = self.relu(self.layer3(x.clone()) + x)
+
+        x = self.relu(self.layer4(x.clone()) + self.layer4_res(x))
+        x = self.relu(self.layer5(x.clone()) + x)
+
+        x = self.relu(self.layer6(x.clone()) + self.layer6_res(x))
+        x = self.relu(self.layer7(x.clone()) + x)
+
+        # x = self.relu(self.layer8(x.clone()) + self.layer8_res(x))
+        # x = self.relu(self.layer9(x.clone()) + x)
+
+        x = self.avg_pool(x)
+        x = self.fc_layer(x)
+
+        return x
+
+def bottleneck3x3(in_channel, out_channel, stride=1):
+    return nn.Sequential(
+        nn.BatchNorm2d(in_channel),
+        nn.ReLU(),
+        nn.Conv2d(in_channel, out_channel, kernel_size=3, stride=stride, padding=1),
+        nn.BatchNorm2d(out_channel),
+        nn.Conv2d(out_channel, out_channel, kernel_size=3, padding=1)
+    )
+
+def bottleneck1x1(in_channel, out_channel, stride=1):
+    return nn.Sequential(
+        nn.BatchNorm2d(in_channel),
+        nn.Conv2d(in_channel, out_channel, kernel_size=1, stride=stride, padding=0),
+    )
+
+def block3x3(in_channel, out_channel):
+    return nn.Sequential(
+        nn.BatchNorm2d(in_channel),
+        nn.ReLU(),
+        nn.Conv2d(in_channel, out_channel, kernel_size=3, padding=1),
+        nn.BatchNorm2d(out_channel),
+        nn.Conv2d(out_channel, out_channel, kernel_size=3, padding=1),
+    )
+
+def identity(out_channel):
+    return nn.BatchNorm2d(out_channel)
+
 class CNN(nn.Module):
     def __init__(self):
         super(CNN, self).__init__()
@@ -38,14 +130,14 @@ class CNN(nn.Module):
             nn.Linear(100, 10)
         )
 
+
+
         # weight initialization
         for m in self.modules():
             if isinstance(m, nn.Conv2d):
+                nn.init.constant_(m.weight, 0)
+            elif isinstance(m, nn.Linear):
                 init.kaiming_normal(m.weight.data)
-                # m.bias.data.fill_(0)
-            if isinstance(m, nn.Linear):
-                init.kaiming_normal(m.weight.data)
-                # m.bias.fill_(0)
 
     def forward(self, x):
         x = self.layer(x)
@@ -57,34 +149,39 @@ def compute_acc(dloader=None, model=None):
     correct = 0
     total = 0
 
-    for i, [imgs, labels] in enumerate(dloader):
-        img = Variable(imgs, requires_grad=False).cuda()
-        label = Variable(labels).cuda()
+    model.eval()
 
-        output = model(img)
+    for i, [imgs, labels] in enumerate(dloader):
+        imgs = imgs.cuda()
+        labels = labels.cuda()
+
+        output = model(imgs)
         _, output_index = torch.max(output, 1)
 
         total += label.size(0)
-        correct += (output_index == label).sum().float()
+        correct += (output_index == labels).sum().float()
 
     print(f'Accuracy of Test Data: {100 * correct/total}')
 
+
 if __name__ == '__main__' :
-    batch_size = 64
-    learning_rate = 1e-3
-    epochs = 10
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--batch_size', default=64, type=int)
+    parser.add_argument('--lr', default=1e-3, type=float)
+    parser.add_argument('--epochs', default=10, type=int)
+
+    args, fire_args = parser.parse_known_args()
+
+    batch_size = args.batch_size
+    learning_rate = args.lr
+    epochs = args.epochs
 
     cifar_train = dsets.CIFAR10(root="CIFAR10/",
                                 train=True,
                                 transform=transforms.Compose([
                                     transforms.ToTensor(),
-                                    # data augmentation
-                                    transforms.Scale(36),
-                                    transforms.CenterCrop(32),
-                                    transforms.RandomHorizontalFlip(),
-                                    transforms.RandomRotation(90),
                                     # data normalization
-                                    transforms.Normalize(mean=(0.5,0.5,0.5), std=(0.5,0.5,0.5))
+                                    # transforms.Normalize(mean=(0.5,0.5,0.5), std=(0.5,0.5,0.5))
                                 ]),
                                 target_transform=None,
                                 download=True)
@@ -94,13 +191,8 @@ if __name__ == '__main__' :
                               transform=transforms.Compose([
                                     transforms.ToTensor(),
                                     # data augmentation
-                                    # test set에서는 data augmentation을 하지 않는다
-                                    # transforms.Scale(36),
-                                    # transforms.CenterCrop(32),
-                                    # transforms.RandomHorizontalFlip(),
-                                    # transforms.Lambda(lambda x: x.rotate(90)),
                                     # data normalization
-                                    transforms.Normalize(mean=(0.5,0.5,0.5), std=(0.5,0.5,0.5))
+                                    # transforms.Normalize(mean=(0.5,0.5,0.5), std=(0.5,0.5,0.5))
                                 ]),
                               target_transform=None,
                               download=True)
@@ -108,12 +200,12 @@ if __name__ == '__main__' :
     train_loader = DataLoader(dataset=cifar_train,
                               batch_size=batch_size,
                               shuffle=True,
-                              num_workers=4,
+                              num_workers=20,
                               drop_last=True)
     test_loader = DataLoader(dataset=cifar_test,
                              batch_size=batch_size,
                              shuffle=False,
-                             num_workers=4,
+                             num_workers=20,
                              drop_last=True)
 
     print(f'cifar_train 길이 : {len(cifar_train)}')
@@ -124,25 +216,33 @@ if __name__ == '__main__' :
     print(f'image data 형태 : {img.shape}')
     print(f'label           : {label}')
 
-    model = CNN().cuda()
+    # model = CNN().cuda()
+    model = my_ResNet().cuda()
     loss_fn = nn.CrossEntropyLoss().cuda()
     optimizer = optim.Adam(model.parameters(), lr=learning_rate)
+    # scheduler = lr_scheduler.StepLR(optimizer, step_size=10, gamma=0.2)
 
     from tqdm import tqdm
     for epoch in range(epochs):
-        for img, label in tqdm(train_loader):
-            x = Variable(img).cuda()
-            y = Variable(label).cuda()
+        model.train()
+
+        for img, label in train_loader:
+            img = img.cuda()
+            label = label.cuda()
 
             optimizer.zero_grad()
-    
-            output = model.forward(x)
-            loss = loss_fn(output, y)
+
+            output = model.forward(img)
+            loss = loss_fn(output, label)
 
             loss.backward()
             optimizer.step()
 
-        if epoch % 1 == 0:
-            print(f'epoch: {epoch:3d}  loss: {loss:4f}')
+        print(f'epoch: {epoch:3d}  loss: {loss:.4f}')
+        compute_acc(test_loader, model=model)
 
-    compute_acc(test_loader, model=model)
+
+    torch.save(model, 'resnet_model1.pth')
+
+
+
